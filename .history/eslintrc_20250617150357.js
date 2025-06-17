d4533749371888f5bb5a201b8c4a23076b2e3c57
Zoran Dimitrievski <132205377+Z0ck0@users.noreply.github.com>
2025-06-17 15:08:59 +0200

    Improvements

---
+++ .history/eslintrc_20250617150357.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  "plugins": ["prettier"],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {
+    "prettier/prettier": [
+      "error",
+      {
+        "printWidth": 100,
+        "tabWidth": 2,
+        "useTabs": false,
+        "semi": true,
+        "singleQuote": false,
+        "trailingComma": "es5",
+        "bracketSpacing": true,
+        "arrowParens": "avoid"
+      }
+    ],
+    "object-curly-newline": ["error", { "consistent": true }],
+    "array-bracket-newline": ["error", { "multiline": true }],
+    "array-element-newline": ["error", { "multiline": true, "minItems": 3 }],
+    "array-element-newline": ["error", "always"],
+
+    "comma-dangle": ["error", "always-multiline"]
+  }
+}
+
+
